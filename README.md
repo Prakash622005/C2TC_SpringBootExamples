9926676efec0632daddb90b09b269648085d4454
Prakash622005 <prakashramesh3839@gmail.com>
2026-01-14 07:52:48 +0530

    Added Spring Boot REST API and CRUD JPA examples

---
+++ README.md
@@ -0,0 +1,9 @@
+# C2TC Spring Boot Examples
+
+This repository contains Spring Boot practice projects demonstrating:
+
+- REST API development
+- CRUD operations
+- Spring Data JPA
+- Database integration
+- API testing using Postman
---
+++ SpringBoot_CRUD_JPA/service/EmployeeService.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public Employee saveEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+}
